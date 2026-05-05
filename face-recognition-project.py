7ac3552c9bf43b118729d1eba23ca45a7b39eeac
Justin Tran <justinqt@gmail.com>
2026-05-05 17:40:39 -0400

    Added the face detection part of cv and add rectangles around faces

--- face-recognition-project.py
+++ face-recognition-project.py
@@ -1,12 +1,25 @@
 import cv2
 # This brings in a pretrained model from a link
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# Had to add the cv2.data.haarcascades because it wasn't working with only the .xml file
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 # This opens my webcam. The zero is the default camera 
 webcam = cv2.VideoCapture(0)
 
 while True:
     # Grabs a frame (image) from my webcam. The underscore is a T/F to see if it worked. We don't care about this
     _, img = webcam.read()
+    
+    # Turns the original image into gray scale image
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # MultiScale checks the image at different sizes
+    # First you pass in the gray scaled image
+    # Parameter 2 is the scale factor; how much you want the image shrunk. Larger value = faster, less accurate
+    # Parameter 3 controls how strict detection is. A face must be detected at least 4 times nearby to count. Higher value = fewer false positives but might miss faces.
+    faces = face_cascade.detectMultiScale(gray, 1.5, 1)
+    
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
     # Displays the image using OpenCV
     cv2.imshow("Face detection", img)
     # This waits 10 milliseconds and checks if a key was pressed
