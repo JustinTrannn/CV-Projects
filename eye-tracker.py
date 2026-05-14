9797c349b3ce9b87c4830b85ebd97f9e54deb5ba
Justin Tran <justinqt@gmail.com>
2026-05-13 20:59:32 -0400

    Changed model to face_landmarker.task

--- eye-tracker.py
+++ eye-tracker.py
@@ -1,13 +1,25 @@
 import cv2
 import mediapipe as mp
 
+# Documentation here: https://ai.google.dev/edge/mediapipe/solutions/vision/face_landmarker/python#video
+BaseOptions = mp.tasks.BaseOptions
+FaceLandmarker = mp.tasks.vision.FaceLandmarker
+FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+options = FaceLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path="face_landmarker.task"),
+    running_mode=VisionRunningMode.VIDEO
+)
+
+landmarker = FaceLandmarker.create_from_options(options)
+
 webcam = cv2.VideoCapture(0)
 
 while True:
     _, image = webcam.read()
     image = cv2.flip(image, 1)
-    rbg_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    mp_image = mp.Image(mp.ImageFormat.SRGB, rbg_image)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     cv2.imshow("Eye tracker", image)
 
